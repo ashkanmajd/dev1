fd0370468339cad215a983c189c39dfcd81e9920
NedaP <NedaP@NedaPC.bonyan.local>
2017-06-17 17:08:57 +0430

    by neda

--- Application1/Project1/src/project1/SessionEJBBean.java
+++ Application1/Project1/src/project1/SessionEJBBean.java
@@ -5,7 +5,10 @@ import javax.annotation.Resource;
 import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 
+import javax.jws.WebService;
+
 @Stateless(name = "SessionEJB", mappedName = "Application1-Project1-SessionEJB")
+@WebService
 public class SessionEJBBean implements SessionEJB, SessionEJBLocal {
     @Resource
     SessionContext sessionContext;
